c8b0e29e7abdc23cbef77746c3ea7870858106c5
websitecuakhoi <khoitk14033@gmail.com>
2025-02-12 20:50:04 +0700

    deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const local_api = "http://localhost:5000";
-const production_api = "https://can-vas.vercel.app";
+const production_api = "https://can-va.vercel.app";
 const token = localStorage.getItem("canva_token");
 const api = axios.create({
   baseURL: production_api,
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(
     cors({
-      origin: "https://can-vas.vercel.app",
+      origin: "https://can-va.vercel.app",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Cung cấp các phương thức HTTP
       allowedHeaders: ["Content-Type", "Authorization"], // Cung cấp các header cần thiết cho request
